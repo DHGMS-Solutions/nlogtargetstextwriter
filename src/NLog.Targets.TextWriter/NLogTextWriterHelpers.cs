e401ea7408fec60b31e4e2c1119ab1d7acd7f99d
David Vreony <dpvreony@users.noreply.github.com>
2018-11-05 00:02:52 +0000

    nlog initialization helpers

---
+++ src/NLog.Targets.TextWriter/NLogTextWriterHelpers.cs
@@ -0,0 +1,76 @@
+﻿using NLog.Config;
+
+namespace NLog.Targets.TextWriter
+{
+    public static class NLogTextWriterHelpers
+    {
+        public static void ConfigureNLogToTextWriter(
+            System.IO.TextWriter textWriter,
+            LogLevel minLevel,
+            string loggerNamePattern = "*")
+        {
+            var loggingConfiguration = new LoggingConfiguration();
+
+            ConfigureNLogToTextWriter(
+                loggingConfiguration,
+                textWriter,
+                minLevel,
+                loggerNamePattern);
+
+            NLog.LogManager.Configuration = loggingConfiguration;
+        }
+
+        public static void ConfigureNLogToTextWriter(
+            System.IO.TextWriter textWriter,
+            LogLevel minLevel,
+            LogLevel maxLevel,
+            string loggerNamePattern = "*")
+        {
+            var loggingConfiguration = new LoggingConfiguration();
+            ConfigureNLogToTextWriter(
+                loggingConfiguration,
+                textWriter,
+                minLevel,
+                maxLevel,
+                loggerNamePattern);
+
+            NLog.LogManager.Configuration = loggingConfiguration;
+        }
+
+        private static void ConfigureNLogToTextWriter(
+            LoggingConfiguration loggingConfiguration,
+            System.IO.TextWriter textWriter,
+            LogLevel minLevel,
+            string loggerNamePattern = "*")
+        {
+            var webJobTarget = new TextWriterTarget(textWriter);
+            loggingConfiguration.AddTarget("TextWriter", webJobTarget);
+
+            var loggingRule = new LoggingRule(loggerNamePattern, minLevel, webJobTarget);
+            loggingConfiguration.LoggingRules.Add(loggingRule);
+
+            WriteInitMessage();
+        }
+
+        private static void ConfigureNLogToTextWriter(
+            LoggingConfiguration loggingConfiguration,
+            System.IO.TextWriter textWriter,
+            LogLevel minLevel,
+            LogLevel maxLevel,
+            string loggerNamePattern = "*")
+        {
+            var webJobTarget = new TextWriterTarget(textWriter);
+            loggingConfiguration.AddTarget("TextWriter", webJobTarget);
+
+            var loggingRule = new LoggingRule(loggerNamePattern, minLevel, maxLevel, webJobTarget);
+            loggingConfiguration.LoggingRules.Add(loggingRule);
+
+            WriteInitMessage();
+        }
+
+        private static void WriteInitMessage()
+        {
+            NLog.LogManager.GetCurrentClassLogger().Debug("Hooked NLog to TextWriter stream.");
+        }
+    }
+}
