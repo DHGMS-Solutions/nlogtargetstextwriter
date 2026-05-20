2317219d50aa73011a01620fef9f7c6113ebec19
David Vreony <dpvreony@users.noreply.github.com>
2026-05-20 22:45:33 +0100

    add fake int test

---
+++ src/NLog.Targets.TextWriter.IntegrationTests/FakeTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace NLog.Targets.TextWriter.IntegrationTests
+{
+    public static class FakeTest
+    {
+        [Fact]
+        public static void DoNothing()
+        {
+            Assert.True(true);
+        }
+    }
+}
